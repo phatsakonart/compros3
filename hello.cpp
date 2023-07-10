0b5c10284f68aef79fa5bdb185ebed4aef3ef343
phatsakonart <139095240+phatsakonart@users.noreply.github.com>
2023-07-10 10:45:39 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;	
+int main()
+{
+	cout<<"	Student Code : 6606021631014"<<endl;
+	cout<<"	Student Name : Phatsakon Chaiyamat"<<endl;
+	cout<<"	    Division : Information Technology"<<endl;
+	cout<<"                Year : 1"<<endl;
+	cout<<"                Room : RB"<<endl;
+	cout<<"                Sex  : Male"<<endl;
+	cout<<"                Age  : 19"<<endl;
+	system("pause");
+	return(0);
+}
